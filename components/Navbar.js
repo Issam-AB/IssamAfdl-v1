660ed18b2d5a0da7f9bc68592d63a4a14cdfadb8
Issam <Issam.aboulfadl05@gmail.com>
2022-01-13 21:00:47 +0100

    finish navbar style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
 	const [menu, setMenu] = useState(true);
@@ -14,69 +15,61 @@ const Navbar = () => {
 				<nav className="bg-[#0a192f] top-0 py-3 fixed inset-x-0 bg-opacity-90 backdrop-blur-sm shadow-xl">
 					<div className="max-w-8xl mx-auto px-12">
 						<div className="flex justify-between">
-							{/* <div className="flex space-x-4 justify-between"> */}
 							{/* <!---- Logo --------> */}
 							<div className="text-white">
-								<a
-									href=""
-									className="flex items-center py-3 px-2 text-gray-500"
-								>
-									{/* <Image /> */}
-									{/* <svg
-										xmlns="http://www.w3.org/2000/svg"
-										className="h-6 w-6 mr-2 text-emerald-400"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-									>
-										<path
-											strokelinecap="round"
-											strokelinejoin="round"
-											strokeWidth="2"
-											d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+								<Link href="/">
+									<a className="flex items-center py-3 px-2 text-gray-500">
+										<Image
+											// loader={myLoader}
+											src="/logo.png"
+											alt="Picture of the author"
+											width={50}
+											height={50}
 										/>
-									</svg> */}
-
-									<span>Issam afdl</span>
-								</a>
+									</a>
+								</Link>
 							</div>
 							{/* <!---- Primary nav --------> */}
-							<div className="hidden md:flex items-center space-x-1">
-								<div className="flex ">
+							<div className="hidden md:flex items-center ">
+								<div className="flex space-x-2 ">
 									<a
-										href=""
-										className=" flex items-center py-2 px-3 text-gray-700"
+										href="#about"
+										className=" flex items-center py-2 px-3 text-[#ccd6f6] hover:text-teal-300 focus:text-teal-300"
 									>
-										<span>01</span>
+										<span className="pr-1 text-teal-300">01.</span>
 										About
 									</a>
 
 									<a
-										href=""
-										className=" flex items-center py-2 px-3 text-gray-700"
+										href="#experience"
+										className=" flex items-center py-2 px-3  text-[#ccd6f6]   hover:text-teal-300 focus:text-teal-300"
 									>
-										<span>01</span>
+										<span className="pr-1 text-teal-300">02.</span>
 										Experience
 									</a>
 
 									<a
-										href=""
-										className="flex items-center py-2 px-3 text-gray-700"
+										href="#work"
+										className="flex items-center py-2 px-3  text-[#ccd6f6]   hover:text-teal-300 focus:text-teal-300"
 									>
-										<span>01</span>
+										<span className="pr-1 text-teal-300">03.</span>
 										Work
 									</a>
 
 									<a
-										href=""
-										className="flex items-center py-2 px-3 text-gray-700"
+										href="contact"
+										className="flex items-center py-2 px-3  text-[#ccd6f6]   hover:text-teal-300 focus:text-teal-300"
 									>
-										<span>01</span>
+										<span className="pr-1 text-teal-300">04.</span>
 										Contact
 									</a>
 
-									<button className="text-emerald-400 border border-solid border-emerald-400 rounded-sm py-2 px-4 font-mono no-underline leading-3 cursor-pointer ml-5 font-light  bg-transparent  duration-300">
-										Resume
+									{/* <button className="text-emerald-400 hover:animate-bounce  border border-solid border-emerald-400 rounded-md py-3 px-4 ml-4  no-underline leading-3 cursor-pointer  font-light  bg-transparent  duration-300"></button> */}
+
+									<button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800">
+										<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+											Resume
+										</span>
 									</button>
 								</div>
 							</div>
@@ -85,7 +78,7 @@ const Navbar = () => {
 								<button onClick={handlClick}>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										className="h-7 w-7 text-emerald-400"
+										className="h-10 w-10 text-emerald-400"
 										fill="none"
 										viewBox="0 0 24 24"
 										stroke="currentColor"
@@ -106,37 +99,39 @@ const Navbar = () => {
 					<div className={` md:hidden ${menu ? "hidden" : ""}`}>
 						<a
 							href=""
-							className=" flex items-center py-2 px-3 text-gray-700 hover:bg-gray-800"
+							className=" flex items-center py-2 px-3 text-[#ccd6f6] hover:text-teal-300 focus:text-teal-300"
 						>
-							<span>01</span>
+							<span className="pr-1 text-teal-300">01.</span>
 							About
 						</a>
 
 						<a
 							href=""
-							className=" flex items-center py-2 px-3 text-gray-700 hover:bg-gray-800"
+							className=" flex items-center py-2 px-3 text-[#ccd6f6] hover:text-teal-300 focus:text-teal-300"
 						>
-							<span>01</span>
+							<span className="pr-1 text-teal-300">02.</span>
 							Experience
 						</a>
 
 						<a
 							href=""
-							className="flex items-center py-2 px-3 text-gray-700 hover:bg-gray-800"
+							className="flex items-center py-2 px-3 text-[#ccd6f6] hover:text-teal-300 focus:text-teal-300"
 						>
-							<span>01</span>
+							<span className="pr-1 text-teal-300">03.</span>
 							Work
 						</a>
 
 						<a
 							href=""
-							className="flex items-center py-2 px-3 text-gray-700 hover:bg-gray-800"
+							className="flex items-center py-2 px-3 text-[#ccd6f6] hover:text-teal-300 focus:text-teal-300"
 						>
-							<span>01</span>
+							<span className="pr-1 text-teal-300">04.</span>
 							Contact
 						</a>
-						<button className="text-emerald-400 border border-solid border-emerald-400 rounded-sm py-2 px-4 font-mono no-underline leading-3 cursor-pointer ml-5 font-light  hover:bg-teal-100 bg-transparent duration-300">
-							Resume
+						<button className="relative inline-flex items-center justify-center p-0.5 mb-2 ml-3 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800">
+							<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+								Resume
+							</span>
 						</button>
 					</div>
 				</nav>
---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<meta
+						name="description"
+						content="Online Marketplace to sell products"
+					/>
+
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin
+					></link>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
